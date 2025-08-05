1c1d543c70ac61ea1dd6df445e30af9f85ddfab4
aakikip <poudelaakriti@gmail.com>
2025-08-05 14:34:50 -0700

    changes partial practice

--- Untitled.R
+++ Untitled.R
@@ -39,3 +39,17 @@ y <- seq(from = _0.4, to = 2.0, by = 0.1)
 
 #evaluate the slope of T(y) at each of those values
 eval(dt_dy)
+#one small change
+
+library(ggplot2)
+library(palmerpenguins)
+ggplot(data = penguins, aes(x = body_mass_g, y = flipper_length_mm)) + 
+  geom_point(aes(color = species)) +
+  scale_color_manual(values = c("darkorange", "purple", "cyan4")) +
+  labs( x = "Body mass (g)",
+        y = "Flipper length(mm)",
+        title = "Palmer penguins size measurements",
+        subtitle = "Palmer archipelago, Antartice (2007 - 2009)",
+        caption = "Collected by Dr. Kristen Gorman") +
+  facet_wrap(~island) +
+  theme_minimal()
